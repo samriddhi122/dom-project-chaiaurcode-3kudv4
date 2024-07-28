bbe959ddfe20257ec5a66f5e72e2e61f1d761987
samriddhi122 <146834083+samriddhi122@users.noreply.github.com>
2024-07-28 14:22:58 +0530

    Initial commit

---
+++ 1-colorChanger/chaiaurcode.js
@@ -0,0 +1,22 @@
+console.log('samriddhi');
+const buttons = document.querySelectorAll('.botton');
+const body = document.querySelector('body');
+buttons.forEach(function (button) {
+  console.log(button);
+  button.addEventListener('click', function (e) {
+    console.log(e);
+    console.log(e.target);
+    if (e.target.id === 'grey') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'white') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'blue') {
+      body.style.backgroundColor = e.target.id;
+    }
+    if (e.target.id === 'yellow') {
+      body.style.backgroundColor = e.target.id;
+    }
+  });
+});
---
+++ README.md
@@ -0,0 +1,3 @@
+# dom-project-chaiaurcode-3kudv4
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/samriddhi122/dom-project-chaiaurcode-3kudv4)
